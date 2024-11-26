ad84e9dd3ebedf193ee219ea311de36b95fdf7ab
Or <Or@MacBook-Pro.local>
2024-11-26 19:06:46 +0200

    hw3 neww

--- Algebra.java
+++ Algebra.java
@@ -107,41 +107,43 @@ public class Algebra {
 
 	// Returns x^n (for n >= 0)
 	public static int pow(int x, int n) {
-		if (n == 0) return 1; 
-		int result = 1;
-		boolean negativeBase = (x < 0) && (n % 2 != 0); 
-	
-		for (int i = 0; i < n; i++) {
-			result = times(result, x); 
+		
+		if (n == 0) {
+			return 1;
 		}
-		if (negativeBase) {
+		boolean isNegative = (x < 0) && (n % 2 != 0);  
+		x = Math.abs(x); 
+		int result = 1;
+		for (int i = 0; i < n; i++) {
+			result = times(result, x);
+		}
+		if (isNegative) {
 			result = -result;
 		}
 	
 		return result;
 	}
+	
+	
 	// Returns the integer part of x1 / x2 
 	public static int div(int x1, int x2) {
-		// מתודה זו מחשבת את החלוקה של x1 ב-x2
-		// מחזירה 0 אם x2 שווה לאפס (לא ניתן לחלק ב-0)
-		// מחזירה את התוצאה (בשלם) של x1/x2
+	
 		
 		if (x2 == 0) {
-			// חלוקה ב-0 אינה אפשרית
+		
 			return 0;
 		}
 		
 		int divi = 0;
-		int num = Math.abs(x1);   // לוקחים את הערך המוחלט של x1
-		int diviser = Math.abs(x2); // לוקחים את הערך המוחלט של x2
-	
-		// נבדוק חלוקה של שני מספרים חיוביים
+		int num = Math.abs(x1); 
+		int diviser = Math.abs(x2); 
+
 		while (num >= diviser) {
-			num = minus(num, diviser); // כל פעם מורידים את x2 מ-x1
+			num = minus(num, diviser); 
 			divi++;
 		}
 	
-		// אם אחד מהמספרים שליליים, התוצאה צריכה להיות שלילית
+
 		if ((x1 < 0 && x2 > 0) || (x1 > 0 && x2 < 0)) {
 			divi = -divi;
 		}
@@ -187,18 +189,15 @@ public class Algebra {
 
 	// Returns the integer part of sqrt(x) 
 	public static int sqrt(int x) {
-		// Replace the following statement with your code
-		int num = x;
-		int sum = 0 ;
+		if (x <= 0) {
+			return 0;
+		}
 		int checker = 1;
-		if (x<=0) {
-			return 0 ;
+		while (pow(checker, 2) <= x) {
+			checker++;  
 		}
-		while (sum < num)
-		{
-			sum = pow(checker,2);
-			checker++;
-		}
-		return minus(checker, 1);
-	}	  	  
+		return checker - 1;
+	}
+	
+	
 }
--- Anagram.java
+++ Anagram.java
@@ -2,103 +2,90 @@ import java.util.Random;
 
 /** Functions for checking if a given string is an anagram. */
 public class Anagram {
-	public static void main(String args[]) {
-		// Tests the isAnagram function.
-		System.out.println(isAnagram("silent","listen"));  // true
-		System.out.println(isAnagram("William Shakespeare","I am a weakish speller")); // true
-		System.out.println(isAnagram("Madam Curie","Radium came")); // true
-		System.out.println(isAnagram("Tom Marvolo Riddle","I am Lord Voldemort")); // true
+    public static void main(String args[]) {
+        // Tests the isAnagram function.
+        System.out.println(isAnagram("silent","listen"));  // true
+        System.out.println(isAnagram("William Shakespeare","I am a weakish speller")); // true
+        System.out.println(isAnagram("Madam Curie","Radium came")); // true
+        System.out.println(isAnagram("Tom Marvolo Riddle","I am Lord Voldemort")); // true
 
-		// Tests the preProcess function.
-		System.out.println(preProcess("What? No way!!!"));
-		
-		// Tests the randomAnagram function.
-		System.out.println("silent and " + randomAnagram("silent") + " are anagrams.");
-		
-		// Performs a stress test of randomAnagram 
-		String str = "1234567";
-		Boolean pass = true;
-		//// 10 can be changed to much larger values, like 1000
-		for (int i = 0; i < 10; i++) {
-			String randomAnagram = randomAnagram(str);
-			System.out.println(randomAnagram);
-			pass = pass && isAnagram(str, randomAnagram);
-			if (!pass) break;
-		}
-		System.out.println(pass ? "test passed" : "test Failed");
-	}  
+        // Tests the preProcess function.
+        System.out.println(preProcess("What? No way!!!"));
+        
+        // Tests the randomAnagram function.
+        System.out.println("silent and " + randomAnagram("silent") + " are anagrams.");
+        
+        // Performs a stress test of randomAnagram 
+        String str = "1234567";
+        Boolean pass = true;
+        //// 10 can be changed to much larger values, like 1000
+        for (int i = 0; i < 10; i++) {
+            String randomAnagram = randomAnagram(str);
+            System.out.println(randomAnagram);
+            pass = pass && isAnagram(str, randomAnagram);
+            if (!pass) break;
+        }
+        System.out.println(pass ? "test passed" : "test Failed");
+    }  
 
+    // Returns a preprocessed version of the given string: all the letter characters are converted
+    // to lower-case, and all the other characters are deleted, except for spaces, which are left
+    // as is. For example, the string "What? No way!" becomes "whatnoway"
+    public static String preProcess(String str) {
+        String newstr1 ="";
+        for (int i = 0; i < str.length(); i++) {
+            if (((str.charAt(i) < 65 || str.charAt(i) > 122)) && str.charAt(i) != 32) {
+                continue;
+            }
+            if (Character.isUpperCase(str.charAt(i))) {
+                newstr1 += Character.toLowerCase(str.charAt(i));
+            } else {
+                newstr1 += str.charAt(i);
+            }
+        }
+        return newstr1;
+    } 
 
-	   
-	// Returns a preprocessed version of the given string: all the letter characters are converted
-	// to lower-case, and all the other characters are deleted, except for spaces, which are left
-	// as is. For example, the string "What? No way!" becomes "whatnoway"
-	public static String preProcess(String str) {
-		String newstr1 ="";
-		for (int i=0; i<str.length();i++)
-		{
-			
-			if (((str.charAt(i)<65||str.charAt(i)>122))&&str.charAt(i)!=32) {
-				continue;
-			}
-			if(Character.isUpperCase(str.charAt(i)))
-			{
-				newstr1+=Character.toLowerCase(str.charAt(i));
-			}
-			else {
-				newstr1+= str.charAt(i);
-			}
+    public static boolean isAnagram(String str1, String str2) {
+        str1 = preProcess(str1);
+        str2 = preProcess(str2);
 
-			}
-		
-		return newstr1;
-	} 
-	   	
-		   public static boolean isAnagram(String str1, String str2) {
-			str1 = preProcess(str1);
-			str2 = preProcess(str2);
-	
-			if (str1.length() != str2.length()) {
-				return false;
-			}
-	
-			int checker = 0;
-	
-			for (int i = 0; i < str1.length(); i++) {
-				for (int j = 0; j < str2.length(); j++) {
-					if (str1.charAt(i) == str2.charAt(j)) {
-						checker++;
-						str2 = str2.substring(0, j) + str2.substring(j + 1);  
-						break;
-					}
-				}
-			}
-	
-			return checker == str1.length();
-		}
+        if (str1.length() != str2.length()) {
+            return false;
+        }
 
-	// Returns a random anagram of the given string. The random anagram consists of the same
-	// characters as the given string, re-arranged in a random order. 
-	public static String randomAnagram(String str) {
-		int length = str.length();
-		String changes = str;
-		Random random = new Random();
-		int randomnum = 0;
-		String newstr = "";
-		
-		
-		do {
-			newstr = ""; 
-			changes = str;  
-			for (int i = 0; i < str.length(); i++) {
-				randomnum = random.nextInt(length);
-				newstr += changes.charAt(randomnum);
-				changes = str.substring(0, randomnum) + str.substring(randomnum + 1);
-				length--;
-			}
-		} while (newstr.equals(str)); 
-		
-		return newstr;
-	}
-	
+        int checker = 0;
+
+        for (int i = 0; i < str1.length(); i++) {
+            char currentChar = str1.charAt(i);
+            for (int j = 0; j < str2.length(); j++) {
+                if (currentChar == str2.charAt(j)) {
+                    checker++;
+                    str2 = str2.substring(0, j) + str2.substring(j + 1);
+                    break;
+                }
+            }
+        }
+
+        return checker == str1.length();
+    }
+
+    // Returns a random anagram of the given string. The random anagram consists of the same
+    // characters as the given string, re-arranged in a random order. 
+    public static String randomAnagram(String str) {
+        int length = str.length();
+        String changes = str;
+        Random random = new Random();
+        int randomnum = 0;
+        String newstr = "";  
+
+        for (int i = 0; i < length; i++) {
+            randomnum = random.nextInt(length); 
+            newstr += changes.charAt(randomnum);  
+            changes = str.substring(0, randomnum) + str.substring(randomnum + 1);
+            length--; 
+        }
+
+        return newstr;  
+    }
 }
